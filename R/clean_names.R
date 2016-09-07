5243295c1ccd5fb4736b163707ba031d5f73f1dc
vincentguyader <vincent@guyader.pro>
2016-09-07 09:49:29 +0200

    clean_names

--- R/clean_names.R
+++ R/clean_names.R
@@ -1,5 +1,9 @@
+#' @export
 
 
+test <- "_ERT_________lkjg___COUCOU_"
+gsub("_+","_",test)
+test
 clean_names<-function(eldata){
   old <- names(eldata)
   names(eldata)<-make.unique(names(eldata))
@@ -8,19 +12,16 @@ clean_names<-function(eldata){
   names(eldata)<-gsub("\\.\\.",".",names(eldata))
   names(eldata)<-gsub("^X.","",names(eldata))
   names(eldata)<-gsub("\\.","_",names(eldata))
-  for (i in 1:5){
-    #c'te honte TODO
-  names(eldata)<-    gsub("__","_",names(eldata))
-  }
+  names(eldata)<-    gsub("_+","_",names(eldata))
   names(eldata)<-gsub("_$","",names(eldata))
   names(eldata)<-tolower(names(eldata))
   names(eldata)<-make.names(names(eldata))
   names(eldata)<-make.unique(names(eldata))
-  
 
-  
-  
+
+
+
   print(data.frame(old=old,new=names(eldata)))
   invisible(eldata)
-  
+
 }
