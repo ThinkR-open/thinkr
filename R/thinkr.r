df188ce573e858da3bb4f7ebc7591714ce64211c
statnmap <statnmap@users.noreply.github.com>
2019-05-15 08:48:27 +0200

    add package level documentation

--- R/thinkr.r
+++ R/thinkr.r
@@ -1,10 +1,8 @@
-#' thinkr
-#'
-#' a package with some (useful) tools
-#' (data manipulation)
-#'
-#'
-#' @encoding UTF-8
-#' @docType package
-#' @name thinkr
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
