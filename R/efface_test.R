2fbcf610c53615cba5819b02384f2a23306196bf
Arnaud Millan <a.millan@fonroche-lighting.com>
2021-03-03 14:16:33 +0000

    initial commit issue6

--- R/efface_test.R
+++ R/efface_test.R
@@ -5,7 +5,7 @@
 #' @importFrom devtools package_file
 #'
 .efface_test <-function(){
-  liste_a_effacer <-   normalizePath(list.files(path=file.path(devtools::package_file(),"tests","testthat"),pattern = "*.test$",full.names = TRUE))
+  liste_a_effacer <- normalizePath(list.files(path=file.path(devtools::package_file(),"tests","testthat"),pattern = "*.test$",full.names = TRUE))
   lapply(liste_a_effacer,message)
   file.remove(liste_a_effacer)
 
