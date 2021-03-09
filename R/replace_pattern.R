badce8702bee4f031f24665d718c0789be646336
statnmap <sebastien@thinkr.fr>
2021-03-09 11:23:28 +0100

    Merge vignettes and reduce dplyr use

--- R/replace_pattern.R
+++ R/replace_pattern.R
@@ -9,24 +9,25 @@
 #' @export
 #'
 #' @examples
-#' library(dplyr)
-#' library(tidyr)
-#' dataset <-
-#' data.frame(a=as.factor(letters)[1:7],b=letters[1:7],c=1:7,stringsAsFactors = FALSE) %>%
-#' unite("fus",a,b,remove=FALSE,sep="")
-#' dataset %>% replace_pattern("a",'"XXX') %>% summary()
+#' dataset <- data.frame(
+#'   col_a = as.factor(letters)[1:7],
+#'   col_b = letters[1:7],
+#'   col_c = 1:7,
+#'   col_d = paste0(letters[1:7], letters[1:7]),
+#'   stringsAsFactors = FALSE
+#' )
+#'
+#' # replace pattern
+#' replace_pattern(dataset, "a", "XXX-")
 #'
 #' # With exact matching
-#' dataset %>% replace_pattern("a",'"XXX', exact = TRUE)
-#'
-#'
-replace_pattern <- function(dataset,pattern,replacement,exact=FALSE){
-  avant <- sapply(dataset,class)
-  if (!exact){
-    # n'importe ou
-
-    out <- data.frame(lapply(dataset,FUN=gsub2,pattern=pattern,replacement=replacement),stringsAsFactors = FALSE)
-  } else{
+#' replace_pattern(dataset, "a", "XXX-", exact = TRUE)
+replace_pattern <- function(dataset, pattern, replacement, exact = FALSE) {
+  avant <- sapply(dataset, class)
+  if (!exact) {
+    # anywhere
+    out <- data.frame(lapply(dataset, FUN = gsub2, pattern = pattern, replacement = replacement), stringsAsFactors = FALSE)
+  } else {
     # correspondance exact avec une case
 
     # # on passe les facteur en character et on les remet apres
@@ -35,7 +36,9 @@ replace_pattern <- function(dataset,pattern,replacement,exact=FALSE){
     # dataset[which(dataset==pattern,arr.ind = TRUE)] <- replacement
     # # on remet les niveau d'avant
 
-    out <- data.frame(lapply(dataset,FUN=gsub2,pattern=paste0("^",pattern,"$"),replacement=replacement),stringsAsFactors = FALSE)
+    out <- data.frame(lapply(dataset, FUN = gsub2,
+                             pattern = paste0("^", pattern, "$"),
+                             replacement = replacement), stringsAsFactors = FALSE)
     # out <- out %>% set_col_type(col_type = avant)
   }
   out <- out %>% set_col_type(col_type = avant)
@@ -45,16 +48,19 @@ replace_pattern <- function(dataset,pattern,replacement,exact=FALSE){
 
 #' from tidyr
 #' @noRd
-col_name <- function (x, default = stop("Please supply column name", call. = FALSE))
-{
-  if (is.character(x))
+col_name <- function(x, default = stop("Please supply column name", call. = FALSE)) {
+  if (is.character(x)) {
     return(x)
-  if (identical(x, quote(expr = )))
+  }
+  if (identical(x, quote(expr = ))) {
     return(default)
-  if (is.name(x))
+  }
+  if (is.name(x)) {
     return(as.character(x))
-  if (is.null(x))
+  }
+  if (is.null(x)) {
     return(x)
+  }
   stop("Invalid column specification", call. = FALSE)
 }
 
@@ -66,18 +72,15 @@ col_name <- function (x, default = stop("Please supply column name", call. = FAL
 #' @encoding UTF-8
 #' @return a data.frame
 #' @export
-#' @importFrom dplyr mutate_at funs
-set_col_type <- function(dataset,col_type){
+#' @importFrom dplyr mutate_at
+set_col_type <- function(dataset, col_type) {
 
   # Pour l'instant on ne fait que les facteurs
-  to_factor <- names(which(col_type=="factor"))
+  to_factor <- names(which(col_type == "factor"))
 
-  if ( length(to_factor)>0){
-  # dataset <- dataset %>% mutate_each_(funs(as.factor),to_factor)
-  dataset <- dataset %>% mutate_at(to_factor,funs(as.factor))
-
-
-
+  if (length(to_factor) > 0) {
+    # dataset <- dataset %>% mutate_each_(funs(as.factor),to_factor)
+    dataset <- dataset %>% mutate_at(to_factor, .funs = as.factor)
   }
   dataset
 }
@@ -91,13 +94,13 @@ set_col_type <- function(dataset,col_type){
 #' @encoding UTF-8
 #' @export
 #'
-gsub2 <- function(x,...){
-  if ( is.numeric(x) | is.date(x) | is.integer(x) | is.POSIXt(x)){
-    return(x)}
-  if (is.factor(x)){
-    levels(x) <- gsub(x=levels(x),...)
+gsub2 <- function(x, ...) {
+  if (is.numeric(x) | is.date(x) | is.integer(x) | is.POSIXt(x)) {
     return(x)
-
   }
-  gsub(x=x,...)
+  if (is.factor(x)) {
+    levels(x) <- gsub(x = levels(x), ...)
+    return(x)
+  }
+  gsub(x = x, ...)
 }
--- tests/testthat/test-replace_pattern.R
+++ tests/testthat/test-replace_pattern.R
@@ -1,27 +1,26 @@
-library(dplyr)
-library(tidyr)
-
+# Test dataset
+dataset <- data.frame(
+  col_a = as.factor(letters)[1:7],
+  col_b = letters[1:7],
+  col_c = 1:7,
+  col_d = paste0(letters[1:7], letters[1:7]),
+  stringsAsFactors = FALSE
+)
 
 test_that("replace_pattern works", {
-
-
-  dataset <- data.frame(a=as.factor(letters)[1:7],b=letters[1:7],c=1:7,stringsAsFactors = FALSE) %>%
-    unite("fus",a,b,remove=FALSE,sep="")
-
-  DF1 <- dataset %>% replace_pattern("a",'"XXX')
+  DF1 <- replace_pattern(dataset, "a", '"XXX')
   # Chaque a doit etre un "XXX
-  expect_equal(DF1[1,'fus'], '"XXX"XXX')
+  expect_equal(DF1[1, "col_d"], '"XXX"XXX')
 
   # Checking if the 1st element of a is still a factor AND "XXX
-  expect_equal(as.character(DF1[1,'a']), '"XXX')
-  expect_true(is.factor(DF1[1,'a']), label = 'Format is not respected - factor')
+  expect_equal(as.character(DF1[1, "col_a"]), '"XXX')
+  expect_true(is.factor(DF1[1, "col_a"]), label = "Format is not respected - factor")
 
   # Checking if the 1st element of b is still a character AND "XXX
-  expect_equal(DF1[1,'b'], '"XXX')
-  expect_true(is.character(DF1[1,'b']), label = 'Format is not respected - character')
+  expect_equal(DF1[1, "col_b"], '"XXX')
+  expect_true(is.character(DF1[1, "col_b"]), label = "Format is not respected - character")
 
   # Checking that exact matching works
-  DF2 <- dataset %>% replace_pattern("a",'"XXX', exact = TRUE)
-  expect_equal(DF2[1,'fus'], 'aa', label = 'Exact matching error')
-
+  DF2 <- replace_pattern(dataset, "a", '"XXX', exact = TRUE)
+  expect_equal(DF2[1, "col_d"], "aa", label = "Exact matching error")
 })
