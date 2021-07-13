53d1f11628e63535ed2e8f3a7230e596c8ddfb60
Benjamin Louis <contact@benjaminlouis-stat.fr>
2021-07-13 12:58:41 +0000

    add assertthat tests in excel_names functions
    - test if n is a numeric and is a whole number (not using is.integer that I find to strict) in  ncol_to_excel
    - test if col_name is a character in excel_to_ncol
    - add a dummy documentation to create `excel_names` family name

--- R/ncol_to_excel.R
+++ R/ncol_to_excel.R
@@ -1,29 +1,42 @@
+#' Get position or excel name of column
+#' 
+#' @name excel_names
+NULL
+
+
 #' return excel column name from a position number
 #'
-#' @param n the colum position
+#' @rdname excel_names
+#'
+#' @param n the column position
 #'
 #' @export
 #' @importFrom assertthat assert_that
-#'
 #' @examples
 #' ncol_to_excel(35)
+#' excel_to_ncol("BF")
+#' excel_col()
+#' ncol_to_excel(1:6)
+#' excel_to_ncol(c('AF', 'AG', 'AH'))
 ncol_to_excel<-function(n){
-  assert_that(is.integer(n))
+  assert_that(all(is.numeric(n)))
+  assert_that(all(round(n) == n))
   excel_col()[n]
 }
 
 
 #' return excel column position number from a column name
 #'
-#' @param col_name the culum name
+#' @rdname excel_names
+#' @param col_name the culumn name
 #'
 #' @export
 #' @importFrom assertthat assert_that
-#'
-#' @examples
-#' excel_to_ncol("BF")
+#' @importFrom dplyr between
 excel_to_ncol<-function(col_name){
-  assert_that(is.character(col_name))
+  assert_that(all(is.character(col_name)))
+  assert_that(all(between(nchar(col_name), 1, 2)))
+  assert_that(all(unlist(strsplit(col_name, split = "")) %in% LETTERS))
   which(excel_col() %in% col_name)
 }
 
@@ -32,16 +45,15 @@ excel_to_ncol<-function(col_name){
 
 #' return all excel column name
 #'
+#' @rdname excel_names
 #' @export
+#'
 #' @importFrom tidyr unite
-#' @examples
-#' excel_col()
-#' ncol_to_excel(6)
-#' excel_to_ncol('AF')
 excel_col <- function(){
   # require(dplyr)
   c(LETTERS,
     expand.grid(LETTERS,LETTERS)[2:1]  %>% tidyr::unite(Var2,Var1,col="col",sep = "") %>% unlist() %>% unname())
 }
+
 
 
